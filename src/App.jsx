a254e6e8178b6f50a33ca95eda5f524e31b2e529
Nithish <nithishm612345@gmail.com>
2025-01-12 21:30:52 +0530

    lazy load update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React, {useContext} from 'react'
+import React, {useContext, lazy, Suspense} from 'react'
 
-import {Header, SearchBar, Icons, Loader, Banners, FeaturedHealth, 
-  LabTest, Achievement, LifeStyle, HealthCheck, UsersFeedback, HowItWorks, SafeLab, FrequentryAskedQuestions} from "./components"
+import {Header, SearchBar, Icons, Loader, Banners, FeaturedHealth} from "./components"
 
 import './styles/style.css'
 
 import { PageConfigContext } from "./services/PageConfig"
 import { LabTestFilterProvider } from './services/FilterPackage'
 import { HealthCheckUpProvider } from './services/FilterPackage'
+
+const LabTest = lazy(() => import('./components/LabTest'))
+const Achievement = lazy(() => import('./components/Achievement'))
+const  LifeStyle= lazy(() => import('./components/LifeStyle'))
+const HealthCheck = lazy(() => import('./components/HealthCheck'))
+const UsersFeedback = lazy(() => import('./components/UsersFeedback'))
+const HowItWorks = lazy(() => import('./components/HowItWorks'))
+const SafeLab = lazy(() => import('./components/SafeLab'))
+const FrequentryAskedQuestions = lazy(() => import('./components/FrequentryAskedQuestions'))
+
+
 
 
 const App = () => {
@@ -27,16 +37,18 @@ const App = () => {
         <HealthCheckUpProvider>
           <FeaturedHealth />
         </HealthCheckUpProvider>
-        <LabTestFilterProvider>
-           <LabTest />
-        </LabTestFilterProvider>
-        <Achievement />
-        <LifeStyle />
-        <HealthCheck />
-        <UsersFeedback />
-        <HowItWorks />
-        <SafeLab />
-        <FrequentryAskedQuestions />
+        <Suspense fallback={<Loader />}>
+          <LabTestFilterProvider>
+            <LabTest />
+          </LabTestFilterProvider>
+          <Achievement />
+          <LifeStyle />
+          <HealthCheck />
+          <UsersFeedback />
+          <HowItWorks />
+          <SafeLab />
+          <FrequentryAskedQuestions />
+        </Suspense>
         </>
       }
   </div>
